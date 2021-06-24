16eef17c7f5b0c8d5153ac00b20980737b8c9ca9
Harrison Tietze <Harrison4192@gmail.com>
2021-06-24 12:32:57 +0900

    Update auto_boxplot.R

--- R/auto_boxplot.R
+++ R/auto_boxplot.R
@@ -3,6 +3,7 @@
 #' @param .data data
 #' @param continuous_outcome continuous y variable. unquoted column name
 #' @param categorical_variable categorical x variable. unquoted column name
+#' @param categorical_facets categorical facet variable. character vector
 #' @param alpha alpha points
 #' @param width width of jitter
 #' @param color_dots dot color
@@ -13,6 +14,7 @@
 auto_boxplot <- function(.data,
                          continuous_outcome,
                          categorical_variable,
+                         categorical_facets = NULL,
                          alpha = .3,
                          width = .15,
                          color_dots = "black",
@@ -25,5 +27,12 @@ auto_boxplot <- function(.data,
                         y = {{continuous_outcome}})) +
     ggplot2::geom_boxplot(color = color_box) +
     ggplot2::geom_jitter(alpha = alpha, width = width,  color = color_dots) +
-    ggthemes::theme_clean()
+    ggthemes::theme_clean() -> p1
+
+  if(!is.null(categorical_facets)){
+    p1 +
+      ggplot2::facet_wrap(categorical_facets) -> p1
+  }
+
+  p1
 }
