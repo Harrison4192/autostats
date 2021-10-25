39b111dcd8ea649213842a8be4cdc7950e4fa60a
Harrison Tietze <Harrison4192@gmail.com>
2021-10-26 02:14:41 +0900

    autostats13

--- build_package.R
+++ build_package.R
@@ -153,6 +153,12 @@ devtools::check()
 devtools::preview_site()
 devtools::load_all()
 devtools::build_vignettes()
+devtools::spell_check()
+devtools::release(check = T)
+
+usethis::use_cran_comments(open = rlang::is_interactive())
+devtools::check_win_devel()
+devtools::check_rhub()
 
 use_r("get_unique_col_names")
 
